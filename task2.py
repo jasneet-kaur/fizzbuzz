b5af9eec3928ec02470020951e540a81c74c388a
jasneet-kaur <jasneetkaurahir@dal.ca>
2019-05-23 07:53:34 -0700

    created function for character count

--- task2.py
+++ task2.py
@@ -1,4 +1,7 @@
 # copied from https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/ 
+
+
+import string
 def getListOfFiles(dirName):
     # create a list of file and sub directories 
     # names in the given directory 
@@ -18,6 +21,29 @@ def getListOfFiles(dirName):
 
 dirNAme='/home/Fizzbuzz-jasneet'
 listOfFiles = getListOfFiles(dirName)
+
+
+
+def depunct(line):
+	for each in string.punctuation:
+		line=line.replace(each,””)
+	return(line)
+
+
+def character_word_count(listofFiles):
+	mydict={}
+	myfile=open(listofFiles,’r’)
+	for line in myfile:
+		line=line.strip()
+		line=depunct(line)
+		line=line.split()
+		for word in line:
+			if word in mydict.keys():
+				pass
+			else:
+				mydict[word]=len(word)
+	return(mydict)
+print(character_word_count(listofFiles))
 
 
 
